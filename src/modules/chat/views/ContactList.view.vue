afdb34ea1b1098123c8ebd7eef1d08e84a60b702
Blaise1030 <blaisetiong1030@gmail.com>
2021-09-10 20:30:29 +0800

    Added tailwind

---
+++ src/modules/chat/views/ContactList.view.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-red-700">Help</div>
+  <div class="p-4">Hello</div>
+  <div class="h-10 text-red-100">Hello</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContactList",
+  setup() {
+    return {};
+  },
+});
+</script>
+
